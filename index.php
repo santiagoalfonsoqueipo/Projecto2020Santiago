92a6fc60f6ee02dd39af3480eaf73a5f5fb878f2
santi <santi@DESKTOP-KNPLAD6>
2020-11-08 12:22:34 +0000

    arreglo login y registro

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ if(empty($_SESSION['usuario'])){
 <head>
 	<title>Login Page</title>
 
-   
+     
+        
 	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
@@ -23,6 +24,10 @@ if(empty($_SESSION['usuario'])){
 
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
+        <link rel="stylesheet" href="assets-modal/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets-modal/css/form-elements.css">
+        
+
 </head>
 <body>
 <div class="container">
@@ -49,11 +54,9 @@ if(empty($_SESSION['usuario'])){
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input  type="password" id="contrasena" name="contrasena" class="form-control" placeholder="password">
+						<input  type="password" id="contraseña" name="contraseña" class="form-control" placeholder="password">
 					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
+				
 					<div class="form-group">
 						<input type="submit" value="Login" class="btn float-right login_btn">
 					</div>
@@ -61,15 +64,69 @@ if(empty($_SESSION['usuario'])){
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
+					Don't have an account?<a class="btn btn-link-1 launch-modal" href="#" data-modal-id="modal-register">Sign up</a>
+       
 				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
+	
 			</div>
 		</div>
 	</div>
 </div>
+
+           <!-- MODAL -->
+        <div class="modal fade" id="modal-register" tabindex="-1" role="dialog" aria-labelledby="modal-register-label" aria-hidden="true">
+        	<div class="modal-dialog">
+        		<div class="modal-content">
+        			
+        			<div class="modal-header">
+        				<button type="button" class="close" data-dismiss="modal">
+        					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        				</button>
+        				<h3 class="modal-title" id="modal-register-label">Sign up now</h3>
+        				<p>Fill in the form below to get instant access:</p>
+        			</div>
+        			
+        			<div class="modal-body">
+        				
+	                    <form id='registerform' role="form" action="" method="post" class="registration-form">
+	                    	<div class="form-group">
+	                    		<label class="sr-only" for="usuario_registro">usuario</label>
+	                        	<input type="text" name="usuario_registro" placeholder="usuario.." class="form-first-name form-control" id="usuario_registro">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="contraseña_registro">contraseña</label>
+	                        	<input type="text" name="contraseña_registro" placeholder="contraseña..." class="form-last-name form-control" id="contraseña_registro">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="nombre">nombre</label>
+	                        	<input type="text" name="nombre" placeholder="nombre..." class="form-email form-control" id="nombre">
+	                        </div>
+                                <div class="form-group">
+	                        	<label class="sr-only" for="apellido">apellido</label>
+	                        	<input type="text" name="apellido" placeholder="apellido..." class="form-email form-control" id="apellido">
+	                        </div>
+                                 <div class="form-group">
+	                        	<label class="sr-only" for="email">email</label>
+	                        	<input type="text" name="email" placeholder="email..." class="form-email form-control" id="email">
+	                        </div>
+                                  <div class="form-group">
+	                        	<label class="sr-only" for="id_referido">id_referido</label>
+	                        	<input type="text" name="id_referido" placeholder="id_referido..." class="form-email form-control" id="id_referido">
+	                        </div>                                 
+	   
+	                        <button type="submit" class="btn">Sign me up!</button>
+	                    </form>
+	                    
+        			</div>
+        			
+        		</div>
+        	</div>
+        </div>
+        <script src="assets-modal/js/jquery-1.11.1.min.js"></script>
+        <script src="assets-modal/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets-modal/js/jquery.backstretch.min.js"></script>
+        <script src="assets-modal/js/scripts.js"></script>
+    
 
 <script type="text/javascript">
 $(document).ready(function() {
@@ -89,6 +146,37 @@ $(document).ready(function() {
                 if (jsonData.success == "1")
                 {
                     location.href = "privado.php";
+                    exit;
+                }
+                else
+                {
+                    alert("No ha podido acceder al área privado porque sus credenciales de acceso no son válidas");
+                    exit;
+                }
+           }
+       });
+     });
+});
+</script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#registerform').submit(function(e) {
+        e.preventDefault();
+		
+        $.ajax({
+            type: 'POST',
+            url: 'register.php',
+            data: $(this).serialize(),
+            success: function(response)
+            {
+                var jsonData = JSON.parse(response);
+
+                // user is logged in successfully in the back-end
+                // let's redirect
+                if (jsonData.success == "1")
+                {
+                    alert("registrado");
                     exit;
                 }
                 else
